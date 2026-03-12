b30758f60d11c23d38144fc22e7727a4917d6dd1
electronjia <neutronfull@protonmail.com>
2026-03-12 13:01:17 -0400

    Trying to fix the resampling, cropping mismatches and inconsistencies, spacing mismatch

--- pre_processing.py
+++ pre_processing.py
@@ -6,9 +6,13 @@ import SimpleITK as sitk
 import pandas as pd
 import numpy as np
 import os
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import h5py
 from tqdm import tqdm 
+import matplotlib.image as mpimg
+import cv2
 
 class PreProcessor:
     def __init__(self):
@@ -73,63 +77,117 @@ class PreProcessor:
 
         return mean, std
 
-    def resample_volume(self, volume, orig_spacing, desired_spacing, resampling_mode):
+    def resample_volume(self, input_volume, label_volume, input_orig_spacing, label_orig_spacing, desired_spacing, input_resampling_mode, label_resamping_mode):
         # Resample the provided volume to the desired spacing using simpleITK
         # Deep learning models work better using uniform spacing
         # it is important to set resampling_mode to correct function nearestneighbor for label volumes
+        # It was found that some of the input and label data pairs may have mismatched spacing and this required using the same image grid. usually this image grid comes from the input array and is used for the label resampling.
 
-        # define original size and new size
-        orig_size = volume.GetSize()
-        new_size = [ int(old_spacing / new_spacing * size) for old_spacing, new_spacing, size in zip(orig_spacing, desired_spacing, orig_size)]
+        # resample the input array first______________________________________________________________________________________________________
+        # define original size and new size. use round() to avoid flooring the values which caused the error with dimension mismatch
+        input_orig_size = input_volume.GetSize()
+        new_size = [ round(int(old_spacing / new_spacing * size)) for old_spacing, new_spacing, size in zip(input_orig_spacing, desired_spacing, input_orig_size)]
         
         # pre-process the input volume
-        new_metadata = sitk.Image(new_size, volume.GetPixelIDValue())
+        new_metadata = sitk.Image(new_size, input_volume.GetPixelIDValue())
+        new_metadata.SetSpacing(desired_spacing)  # copyinformation() function does not work
+        new_metadata.SetOrigin(input_volume.GetOrigin())
+        new_metadata.SetDirection(input_volume.GetDirection())
         new_metadata.SetSpacing(desired_spacing)
-        new_metadata.SetOrigin(volume.GetOrigin())
-        new_metadata.SetDirection(volume.GetDirection())
 
         # define resampler
         resampler = sitk.ResampleImageFilter()
         resampler.SetReferenceImage(new_metadata)
-        resampler.SetInterpolator(resampling_mode)
+        resampler.SetInterpolator(input_resampling_mode)
         resampler.SetTransform(sitk.Transform())
 
         # resample the input volume
-        resampled_volume = resampler.Execute(volume)
+        resampled_input_volume = resampler.Execute(input_volume)
 
         # get array from the resampled input volume
-        resampled_array = sitk.GetArrayFromImage(resampled_volume)
+        resampled_input_array = sitk.GetArrayFromImage(resampled_input_volume)
 
         # transpose to (x,y,z)
-        resampled_array = np.transpose(resampled_array, (2,1,0))
+        resampled_input_array = np.transpose(resampled_input_array, (2,1,0))
 
-        return resampled_volume, resampled_array
+        # resample the label next using the image grid from input array___________________________________________________________________________________________
+        resampler.SetReferenceImage(resampled_input_volume)
+
+        # resample the label volume (xyz) using the proper resampling mode for the label
+        resampler.SetInterpolator(label_resamping_mode)
+        resampled_label_volume = resampler.Execute(label_volume)
+
+        # get array from resampled label volume -> zyx
+        resampled_label_array = sitk.GetArrayFromImage(resampled_label_volume)
+        
+        # transpose back ->  xyz
+        resampled_label_array = np.transpose(resampled_label_array, (2,1,0))
+
+        assert resampled_input_array.shape == resampled_label_array.shape , f'The resampled input and label shape do not match: {resampled_input_array.shape=} and {resampled_label_array.shape}'
+        assert resampled_input_volume.GetSpacing() == resampled_label_volume.GetSpacing(), f"The resampled input and label spacing do not match: {resampled_input_volume.GetSpacing()=} and {resampled_label_volume.GetSpacing()=}"
+
+        return resampled_input_volume, resampled_input_array, resampled_label_volume, resampled_label_array
 
     def final_channel_dimension(self, array, transpose_function):
         # the final channel dimension should be z,y,x to fit with the pytorch
         return transpose_function(array)
     
-    def display_for_debugging(self, input_array, label_array, axes=['z'], num_slices=1, slice_number=None, processing_type="resampling"):
+    def save_overlay_for_debugging(self, input_path, label_path):
+        # load the newly saved input and label arrays using matplotlib and create an overlay figure
+
+        # laod images as RGBA (RGB+alpha)
+        input_img = mpimg.imread(input_path)
+        label_img = mpimg.imread(label_path)
+
+        # convert input RGBA → grayscale
+        if input_img.ndim == 3:
+            input_img = input_img[..., :3].mean(axis=2)
+
+        # convert label RGBA → mask
+        # if label was saved as RGBA (using cmap='jet), convert to a grayscale mask
+        if label_img.ndim == 3:
+            label_mask = label_img[..., :3].mean(axis=2)
+        else:
+            label_mask = label_img
+
+        overlay_path = "_".join(["debugging/overlay"] + input_path.split("_")[1:])
+
+        # normalize input to 0-255
+        input_img = (input_img * 255).astype(np.uint8)
+
+        # convert grayscale → BGR
+        input_img = cv2.cvtColor(input_img, cv2.COLOR_GRAY2BGR)
+
+        # create colored mask
+        mask = np.zeros_like(input_img)
+        mask[label_mask > 0] = [0,0,255]   # red mask
+
+        # blend
+        overlay = cv2.addWeighted(input_img, 1.0, mask, 0.5, 0)
+
+        cv2.imwrite(overlay_path, overlay)
+
+    def save_plots_for_debugging(self, input_array, label_array, axis='z', slice_number=None, processing_type="resampling"):
+        # saves a single slice from z axis in the middle
+
+        # get middle z slice to save
+        shape = input_array.shape
+        idx = shape[2]  // 2 if slice_number is None else slice_number
+        input_slice = input_array[:,:,idx]
+        label_slice = label_array[:,:,idx]
         
-        # index for dimension
-        idx_dict = {'x':0, 'y':1, 'z':2}
+        input_path = f"debugging/input_{processing_type}_{axis}_{idx}.png"
+        label_path = f"debugging/label_{processing_type}_{axis}_{idx}.png"
 
-        for axis in axes:
-            # select axis index
-            shape = input_array.shape
-            for i in range(num_slices):
-                # pick slice indices evenly
-                if axis in idx_dict:
-                    idx = shape[idx_dict[axis]] * (i+1) // (num_slices+1) if slice_number is None else slice_number
-                    input_slice = input_array[:,:,idx]
-                    label_slice = label_array[:,:,idx]
-                else:
-                    raise ValueError("Axis must be 'x', 'y', or 'z'.")
+        plt.imsave(input_path, input_slice, cmap='gray')
+        plt.imsave(label_path, label_slice, cmap='gray')
+        plt.close("all")
 
-                plt.imsave(f"debugging/input_{processing_type}_{axis}_{idx}.png", input_slice, cmap='gray')
-                plt.imsave(f"debugging/label_{processing_type}_{axis}_{idx}.png", label_slice, cmap='jet')
+        self.save_overlay_for_debugging(input_path=input_path, label_path=label_path)
 
-    def crop_volume(self, input_array, label_array, new_shape, slice_number=None, to_print=True):
+        return input_path, label_path
+
+    def crop_volume(self, input_array, label_array, new_shape):
 
         old_shape = input_array.shape
 
@@ -147,11 +205,12 @@ class PreProcessor:
 
         for idx, (min_idx, max_idx, oldie_shape, desired_shape) in enumerate(zip(mins, maxs, old_shape, new_shape)):
 
+                # tried to get the center is the old shape > desired shape
                 center = (min_idx + max_idx) // 2
                 start = max(0, center - desired_shape // 2)
                 end = start + desired_shape
 
-                # if crop exceeds dimension, shift
+                # gets the top-left part is the old shape < desired shape, then pads
                 if end > oldie_shape:
                     start = max(0, oldie_shape - desired_shape)
                     end = start + desired_shape
@@ -162,16 +221,15 @@ class PreProcessor:
         cropped_label_array = label_array[slices[0], slices[1], slices[2]]
 
         # pad if needed
+        # print(f"{cropped_input_array.shape=}, {cropped_label_array.shape=}") # if troubleshooting the sizes
         pad_width = [(0, max(0, desired - cropped)) for desired, cropped in zip(new_shape, cropped_input_array.shape)]
         if any(pad > 0 for _, pad in pad_width):
             cropped_input_array = np.pad(cropped_input_array, pad_width, mode='constant', constant_values=0)
             cropped_label_array = np.pad(cropped_label_array, pad_width, mode='constant', constant_values=0)
 
-        # debugging
-        if to_print:
-            self.display_for_debugging(cropped_input_array, cropped_label_array, slice_number=slice_number, processing_type="cropping")
+        # print(f"{cropped_input_array.shape=}, {cropped_label_array.shape=}") # if troubleshooting the sizes
 
-        return cropped_input_array, cropped_label_array
+        return cropped_input_array, cropped_label_array, slices, pad_width
 
     def process_all_volumes(self, to_print=True, to_debug=False):
         # Read the excel file containing the file paths
@@ -232,36 +290,47 @@ class PreProcessor:
                 input_mean, input_std = self.get_pos_volume_array_stats(input_array)
 
                 # 2️⃣ resample the input and label volumes to the desired spacing
-                resampled_input_volume, resampled_input_array = self.resample_volume(input_volume, input_spacing, desired_spacing, sitk.sitkBSpline)
-                resampled_label_volume, resampled_label_array = self.resample_volume(label_volume, label_spacing, desired_spacing, sitk.sitkNearestNeighbor)
+                resampled_input_volume, resampled_input_array, resampled_label_volume, resampled_label_array  = self.resample_volume(input_volume, label_volume, input_spacing, label_spacing, desired_spacing, sitk.sitkBSpline,sitk.sitkNearestNeighbor )
 
                 resampled_input_spacing, resampled_input_size = self.get_volume_info(resampled_input_volume) # get spacing and size of the resampled input volume
                 resampled_label_spacing, resampled_label_size = self.get_volume_info(resampled_label_volume) # get spacing and size of the resampled label volume
                 resampled_seg_extent = self.get_segmentation_extent(resampled_label_array) # get segmentation extent of the resampled label volume
 
-                #  🐞 define a slice to debug            
-                old_z_slice = resampled_input_array.shape[2] // 2+10
-                phys_z_slice = input_spacing[2] * old_z_slice
-                new_z_slice = int(round(phys_z_slice / desired_spacing[2]))
+                # 3️⃣crop and pad the resampled input and label arrays to the desired crop size
+                cropped_input_array, cropped_label_array, slices, pad_width = self.crop_volume(resampled_input_array, resampled_label_array, new_shape)
+
+                cropped_seg_extent = self.get_segmentation_extent(cropped_label_array) # get segmentation extent of the cropped label volume
+
+                #  🐞 define a slice to debug  
+                crop_z_start, crop_z_end = slices[-1].start, slices[-1].stop
+                resampled_depth =  resampled_input_array.shape[-1]   
+                resampled_z_slice = resampled_depth // 2
+                cropped_z_slice = (crop_z_start + min(resampled_depth, crop_z_end)) // 2
+
+                phys_slice = resampled_z_slice * desired_spacing[2]
+                orig_z_slice = int(round(phys_slice / input_spacing[2]))
+
+                #  🐞 to debug the z slice definition for orig input, resampled input, cropped input
+                # print(f"{slices=}")
+                # print(f"{crop_z_start=}, {crop_z_end=}, {resampled_depth=}")
+                # print(f"{input_array.shape=}, {resampled_input_array.shape=}, {cropped_input_array.shape=}")
+                # print(f"{orig_z_slice=}, {resampled_z_slice=}, {cropped_z_slice=}")
+
 
                 if to_print:
-                    self.display_for_debugging(input_array, label_array, slice_number=old_z_slice, processing_type="")
-                    self.display_for_debugging(resampled_input_array, resampled_label_array, slice_number=new_z_slice, processing_type="resampling")
+                    self.save_plots_for_debugging(input_array, label_array, slice_number=orig_z_slice, processing_type=f"new={resampled_z_slice}")
+                    self.save_plots_for_debugging(resampled_input_array, resampled_label_array, slice_number=resampled_z_slice, processing_type="resampling")
+                    self.save_plots_for_debugging(cropped_input_array, cropped_label_array, slice_number=cropped_z_slice, processing_type="cropping")
 
-
-                # 3️⃣crop and pad the resampled input and label arrays to the desired crop size
-                cropped_input, cropped_label = self.crop_volume(resampled_input_array, resampled_label_array, new_shape, slice_number=new_z_slice, to_print=to_print)
-
-                cropped_seg_extent = self.get_segmentation_extent(cropped_label) # get segmentation extent of the cropped label volume
-
+                
                 # 4️⃣ normalize image                
-                cropped_input[cropped_input < 0] = 0 # set the negative volume to 0
-                cropped_input = cropped_input / (input_std + 1e-8)
+                cropped_input_array[cropped_input_array < 0] = 0 # set the negative volume to 0
+                cropped_input_array = cropped_input_array / (input_std + 1e-8)
 
                 # 5️⃣ final channel dimension and add channel dimension for pytorch
-                final_input = self.final_channel_dimension(cropped_input, lambda x: np.transpose(x, (2,1,0))) # transpose to ( z, y, x)
+                final_input = self.final_channel_dimension(cropped_input_array, lambda x: np.transpose(x, (2,1,0))) # transpose to ( z, y, x)
                 final_input = final_input[np.newaxis, ...] # add channel dimension to the input array, so the shape becomes (1, z, y, x) for pytorch
-                final_label = self.final_channel_dimension(cropped_label, lambda x: np.transpose(x, (2,1,0))) # transpose to ( z, y, x)
+                final_label = self.final_channel_dimension(cropped_label_array, lambda x: np.transpose(x, (2,1,0))) # transpose to ( z, y, x)
 
                 # populate X_vol and Y_vol arrays
                 X_vol[index, 0, ...] = final_input
@@ -300,8 +369,10 @@ class PreProcessor:
                     'resampled_label_physical_size': np.array(resampled_input_spacing) * (np.array(resampled_seg_extent[1::2])- np.array(resampled_seg_extent[0::2])),
                     'resampled_label_bounding_box_size': (np.array(resampled_seg_extent[1::2])- np.array(resampled_seg_extent[0::2])),
 
-                    'cropped_input_shape': cropped_input.shape,
-                    'cropped_label_shape': cropped_label.shape,
+                    'cropped_input_shape': cropped_input_array.shape,
+                    'cropped_label_shape': cropped_label_array.shape,
+                    'cropping_slices': slices,
+                    'cropping_pad_width': pad_width,
                     'cropped_label_extent': cropped_seg_extent,
                     'cropped_label_bounding_box_size': (np.array(cropped_seg_extent[1::2])- np.array(cropped_seg_extent[0::2])),
 
@@ -325,7 +396,7 @@ class PreProcessor:
                 f.create_dataset("info", data=np.string_(str(volume_info_dict)), compression="gzip")
 
             if to_print:
-                print(f"✅ Saved {N} cases of {unique_dataset} ({object_name}) to {save_path}")
+                print(f"✅ Saved {N} cases of {unique_dataset} ({object}) to {save_path}")
 
 
 if __name__ == "__main__":
